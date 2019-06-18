3109dd571f787faa0b2b9cf5b11aa7593a2ef562
Jonathan Espinosa <jonathan.espinosa@gmail.com>
2019-06-17 19:37:20 -0700

    added delete and confirms

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 export default {
-  // API_ENDPOINT: 'https://medialog-server.herokuapp.com/api',
-  API_ENDPOINT:  'http://localhost:8000/api',
-  // AUTH_ENDPOINT: 'https://medialog-server.herokuapp.com/api/auth/login',
-  AUTH_ENDPOINT: 'http://localhost:8000/api/auth/login',
+  API_ENDPOINT: 'https://medialog-server.herokuapp.com/api',
+  // API_ENDPOINT:  'http://localhost:8000/api',
+  AUTH_ENDPOINT: 'https://medialog-server.herokuapp.com/api/auth/login',
+  // AUTH_ENDPOINT: 'http://localhost:8000/api/auth/login',
   MOVIE_DB_ENDPOINT: 'https://api.themoviedb.org/3/search',
   MOVIE_DB_API_KEY: '3f9f101e64f5c0346d9d4e3a9c63fc10',
 }
